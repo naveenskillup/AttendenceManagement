d8fe1989ef467100413d7351ee5c776e5ed7cd4b
naveenskillup <naveenb1195@gmail.com>
2025-02-07 05:26:00 +0530

    initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AttendenceManagement.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
