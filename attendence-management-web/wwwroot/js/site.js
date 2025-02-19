6c321e65e97a5319d212f72fd53e61c73125275f
naveenskillup <naveenb1195@gmail.com>
2025-02-19 21:49:02 +0530

    fix: enhance spacing

---
+++ attendence-management-web/wwwroot/js/site.js
@@ -0,0 +1,18 @@
+﻿$(document).ready(function () {
+	$('.datepicker').datepicker({
+		format: 'dd-mm-yyyy',  // Set the format you want
+		autoclose: true,
+		todayHighlight: true,
+		startDate: new Date()
+	}).on('changeDate', function (e) {
+		$(this).closest('.input-group').find('.datepicker-input').val(e.format());
+	});
+	$(".datepicker-input").inputmask({
+		mask: "99/99/9999",
+		alias: "datetime",
+		inputFormat: "dd/mm/yyyy",
+		placeholder: "dd/mm/yyyy",
+		clearIncomplete: true // Prevents partial dates like "12/3_/_"
+	});
+
+});
